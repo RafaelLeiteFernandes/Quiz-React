2f55adab6da2f2d82b0c8fc6d52fdc48310c49dd
Rafaelzera7 <rafael.fernandesAIMEC@gmail.com>
2022-10-05 15:38:53 -0300

    tela de perguntas

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
+import {useContext, useEffect} from "react";
+import {QuizContext} from "./context/quiz";
 
-import {Welcome} from './components/Welcome'
+import Welcome from './components/Welcome'
+import { Questions } from "./components/Questions"
+
 import './App.css'
 
 function App() {
+  const [quizState, dispatch] = useContext(QuizContext);
+
+  useEffect(() =>{
+    dispatch({type: "REODER_QUESTIONS"})
+    
+  }, [])
 
   return (
     <div className="App">
      <h1>Quiz de Programação</h1>
-     <Welcome />
+     {quizState.gameStage === "start" && <Welcome />}
+     {quizState.gameStage === "playing" && <Questions />}
     </div>
   )
 }
--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -6,17 +6,18 @@ import Quiz from "../img/quiz.svg";
 import "./Welcome.css";
 
 export function Welcome () {
-  const quizState = useContext(QuizContext);
+  const [quizState, dispatch] = useContext(QuizContext);
 
-  
+ 
 
   return (
     <div id="welcome">
         <h2>Seja bem-vindo !</h2>
         <p>Clique no botão para iniciar:</p>
-        <button>Iniciar</button>
+        <button onClick={() => dispatch({type:"CHANGE_STATE"})}>Iniciar</button>
         <img src={Quiz} alt="Inicio do quiz" />
     </div>
   )
 }
 
+export default Welcome
